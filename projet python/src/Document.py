daaa53704a5f52b84adcf7d62beeb4024c318edd
📊 OUSSAMA SEDDIK <140451607+Oussamaseddik@users.noreply.github.com>
2025-12-06 18:25:13 +0100

    Update Document.py

--- projet python/src/Document.py
+++ projet python/src/Document.py
@@ -1,4 +1,4 @@
-# src/Document.py
+
 class Document:
     def __init__(self, titre, auteur, date, url, texte, source="local"):
         self.titre = titre
@@ -11,7 +11,7 @@ class Document:
     def __str__(self):
         return f"{self.titre} — {self.auteur}"
 
-# Classes spécialisées (si besoin)
+#classes specialisees
 class RedditDocument(Document):
     def __init__(self, titre, auteur, date, url, texte, comments=0):
         super().__init__(titre, auteur, date, url, texte, source="reddit")
@@ -21,3 +21,4 @@ class ArxivDocument(Document):
     def __init__(self, titre, auteurs, date, url, texte):
         super().__init__(titre, ", ".join(auteurs), date, url, texte, source="arxiv")
         self.coauthors = auteurs
+
