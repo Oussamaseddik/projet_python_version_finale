edac236fbc8d4a3aab56e27d0c52d828a1157e83
📊 OUSSAMA SEDDIK <140451607+Oussamaseddik@users.noreply.github.com>
2025-12-10 14:40:44 +0100

    Update factory.py

--- projet python/src/factory.py
+++ projet python/src/factory.py
@@ -3,7 +3,8 @@ from src.Document import RedditDocument, ArxivDocument, Document
    # cree et retourne un document Reddit avec le nombre de commentaires
 class DocumentFactory: 
     def create_reddit(self, titre, auteur, date, url, texte, comments=0):
-         return RedditDocument(titre, auteur, date, url, texte, comments)
+      
+           return RedditDocument(titre, auteur, date, url, texte, comments)
          
 
     def create_arxiv(self, titre, auteurs, date, url, texte): 
@@ -11,3 +12,4 @@ class DocumentFactory:
     # cree et retourne un document generique
     def create_basic(self, titre, auteur, date, url, texte): 
         return Document(titre, auteur, date, url, texte)
+
